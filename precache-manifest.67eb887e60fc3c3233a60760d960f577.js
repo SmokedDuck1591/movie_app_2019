603026197acb0873e4af50ac7c6fe88b71697431
pedro-geun <coutinho159123@gmail.com>
2020-09-04 00:49:40 +0900

    Updates

---
+++ precache-manifest.67eb887e60fc3c3233a60760d960f577.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7dfa9ab9769433b4d0a59f91f25ab24",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "e0a8005d7b6f9715dd5f",
+    "url": "/movie_app_2019/static/css/main.a96a331c.chunk.css"
+  },
+  {
+    "revision": "4d7c806d0e258b226914",
+    "url": "/movie_app_2019/static/js/2.00a09d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.00a09d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a8005d7b6f9715dd5f",
+    "url": "/movie_app_2019/static/js/main.111ec1b1.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
